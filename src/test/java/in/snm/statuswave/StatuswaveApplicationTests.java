0567ff4631546aa3a7a62f1f19224e5ab4ac8a6c
Shahid Nazir Z <shahidnazirmhd@gmail.com>
2024-09-23 05:41:52 +0000

    Update

--- src/test/java/in/snm/statuswave/StatuswaveApplicationTests.java
+++ src/test/java/in/snm/statuswave/StatuswaveApplicationTests.java
@@ -1,10 +1,10 @@
-package in.snm.moraceae;
+package in.snm.statuswave;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MoraceaeApplicationTests {
+class StatuswaveApplicationTests {
 
 	@Test
 	void contextLoads() {
